c87a02f2035cd884e0359895056afe6881eff9ba
Lovepreet <lovepreet.singh@medtrail.in>
2021-02-13 22:25:54 +0530

    add request logger middleware

--- server.js
+++ server.js
@@ -8,6 +8,30 @@ app.use(bodyParser.json({limit: '10mb'}));
 app.use(bodyParser.urlencoded({extended: false}));
 
 
+let httpLogger = (req, res, next) => {
+  let current_datetime = new Date();
+  let formatted_date =
+    current_datetime.getFullYear() +
+    "-" +
+    (current_datetime.getMonth() + 1) +
+    "-" +
+    current_datetime.getDate() +
+    " " +
+    current_datetime.getHours() +
+    ":" +
+    current_datetime.getMinutes() +
+    ":" +
+    current_datetime.getSeconds();
+  let method = req.method;
+  let url = req.url;
+  let status = res.statusCode;
+  let log = `[${formatted_date}] ${method}:${url} ${status}`;
+  console.log(log);
+  next();
+};
+app.use(httpLogger);
+
+
 app.use('/word', wordRouter);
 
 const PORT = 7007;
